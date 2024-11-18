f79b8d63669074d6b44e613335617db46692bec3
Mashiro <thomas.goliu@gmail.com>
2024-11-18 04:17:27 -0500

    better animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function surrender() {
     const receipt = document.getElementById("receipt");
-    const uuid = crypto.randomUUID(); // Generate a unique UUID
+    const uuid = generateUUID(); // Generate a UUID
     const date = new Date().toLocaleString();
 
     document.getElementById("uuid").textContent = uuid;
     document.getElementById("date").textContent = date;
 
     receipt.style.display = "block"; // Show the receipt
+    receipt.scrollIntoView({ behavior: 'smooth' });
 }
