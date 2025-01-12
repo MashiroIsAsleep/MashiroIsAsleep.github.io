6b90b0433ff80cbf72c310c0ab1de785ecaa3103
Mashiro <thomas.goliu@gmail.com>
2025-01-12 16:04:00 -0500

    config

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Mashiro\'s Blog',
+  subtitle: 'uwu',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
     src: '/src/assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
+      enable: true,         // Display the credit text of the banner image
       text: '',              // Credit text to be displayed
       url: ''                // (Optional) URL link to the original artwork or artist's page
     }
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: '/src/assets/images/mypfp.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'MashiroIsAsleep',
+  name: 'Mashiro',
   bio: '17, NY, Student. Aka Demian',
   links: [
     {
